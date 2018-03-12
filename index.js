e91f42a8e278cc67d213ae0f21b2c6f8e7521ae3
jedrzejchalubek <jedrzej.chalubek@gmail.com>
2018-03-12 14:27:45 +0100

    Do not require empty line before comment that is first in the block

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ module.exports = {
   "rules": {
     "at-rule-no-unknown": null,
     "scss/at-rule-no-unknown": true,
-    "scss/double-slash-comment-empty-line-before": "always",
+    "scss/double-slash-comment-empty-line-before": ["always", {
+      "except": ["first-nested"]
+    }],
     "scss/dollar-variable-colon-space-after": "always",
     "scss/dollar-variable-colon-space-before": "never",
     "scss/at-import-no-partial-leading-underscore": true
